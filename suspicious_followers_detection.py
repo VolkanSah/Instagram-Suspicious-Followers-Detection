cbeda331f4a7b98668072941fd24d94ad88a0c5e
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-04-01 00:04:18 +0200

    Update suspicious_followers_detection.py

--- suspicious_followers_detection.py
+++ suspicious_followers_detection.py
@@ -1,8 +1,13 @@
 # Instagram Suspicious Followers Detection- Copyright Volkan Kücükbudak
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 username = 'your_username'
 password = 'your_password'
